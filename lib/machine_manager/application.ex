32c7b458de2e1def1ede531481a641bd0631ca57
Ivan Kozik <ivan@ludios.org>
2017-01-01 09:20:22 +0000

    Add Ecto

---
+++ lib/machine_manager/application.ex
@@ -0,0 +1,19 @@
+defmodule MachineManager.Application do
+	@moduledoc false
+
+	use Application
+
+	def start(_type, _args) do
+		import Supervisor.Spec, warn: false
+
+		# Define workers and child supervisors to be supervised
+		children = [
+			supervisor(MachineManager.Repo, []),
+		]
+
+		# See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
+		# for other strategies and supported options
+		opts = [strategy: :one_for_one, name: MachineManager.Supervisor]
+		Supervisor.start_link(children, opts)
+	end
+end
---
+++ lib/machine_manager/repo.ex
@@ -0,0 +1,3 @@
+defmodule MachineManager.Repo do
+	use Ecto.Repo, otp_app: :machine_manager
+end
