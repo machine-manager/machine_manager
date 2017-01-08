cd5ce21425abec12c87ebaf73c32b51ca9b372dc
Ivan Kozik <ivan@ludios.org>
2017-01-08 08:16:28 +0000

    Make `add` and `list` work, sort of

--- lib/machine_manager.ex
+++ lib/machine_manager.ex
@@ -1,4 +1,6 @@
 defmodule MachineManager do
+	import Ecto.Query, only: [from: 2]
+
 	def transfer(_machine, _file) do
 		# rsync to /root/.cache/machine_manager/#{basename file}
 	end
@@ -7,10 +9,35 @@ defmodule MachineManager do
 		transfer(machine, script)
 		# ssh and run
 	end
+
+	def add(hostname, ip, ssh_port, tags) do
+		MachineManager.Repo.insert_all("machines", [
+			[hostname: hostname, ip: ip_to_inet(ip), ssh_port: ssh_port, tags: tags]
+		])
+	end
+
+	def list() do
+		out = MachineManager.Repo.all(
+			from m in "machines",
+			select: %{hostname: m.hostname, ip: m.ip, ssh_port: m.ssh_port, tags: m.tags}
+		)
+		IO.inspect(out)
+	end
+
+	defp ip_to_inet(ip) do
+		%Postgrex.INET{address: ip_to_tuple(ip)}
+	end
+
+	defp ip_to_tuple(ip) do
+		ip
+		|> String.split(".")
+		|> Enum.map(&String.to_integer/1)
+		|> List.to_tuple
+	end
 end
 
 defmodule MachineManager.CLI do
-	def main(opts) do
+	def main(argv) do
 		spec = Optimus.new!(
 			name:               "machine_manager",
 			description:        "Machine Manager",
@@ -27,10 +54,21 @@ defmodule MachineManager.CLI do
 						ssh_port: [short: "-p", long: "--ssh-port", required: true,  parser: :integer],
 						tag:      [short: "-t", long: "--tag",      required: false, multiple: true],
 					]
+				],
+				list: [
+					name:  "list",
+					about: "List all machines",
+				],
+				probe: [
+					name:  "probe",
+					about: "Probe all machines",
 				]
 			]
 		)
-		options = Optimus.parse!(spec, opts)
-		IO.inspect(options)
+		{[subcommand], %{options: options}} = Optimus.parse!(spec, argv)
+		case subcommand do
+			:add  -> MachineManager.add(options.hostname, options.ip, options.ssh_port, options.tag)
+			:list -> MachineManager.list()
+		end
 	end
 end
