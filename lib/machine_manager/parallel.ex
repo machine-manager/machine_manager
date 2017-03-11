62a6b3f4d342f67d63bc63cb793135ddd5668f8f
Ivan Kozik <ivan@ludios.org>
2017-03-11 01:57:16 +0000

    Add missing lib/machine_manager/parallel.ex

---
+++ lib/machine_manager/parallel.ex
@@ -0,0 +1,22 @@
+defmodule MachineManager.Parallel do
+	@spec block_on_tasks(map, (String.t, term -> term), (map -> term), integer) :: nil
+	def block_on_tasks(task_map, completion_fn, waiting_fn, check_interval) do
+		pid_to_task_name =
+			task_map
+			|> Enum.map(fn {task_name, task} -> {task.pid, task_name} end)
+			|> Map.new
+		waiting_task_map = for {task, result} <- Task.yield_many(task_map |> Map.values, check_interval) do
+			task_name = pid_to_task_name[task.pid] || \
+				raise RuntimeError, message: "task_name == nil for #{inspect task}"
+			case result do
+				{:ok, task_result} -> completion_fn.(task_name, {:ok, task_result}); nil
+				{:exit, reason}    -> completion_fn.(task_name, {:exit, reason});    nil
+				nil                -> {task_name, task}
+			end
+		end |> Enum.reject(&is_nil/1) |> Map.new
+		if waiting_task_map != %{} do
+			waiting_fn.(waiting_task_map)
+			block_on_tasks(waiting_task_map, completion_fn, waiting_fn, check_interval)
+		end
+	end
+end
