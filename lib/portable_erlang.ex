fad7b9070f021b1d37da2693fb2ec4f5830088eb
Ivan Kozik <ivan@ludios.org>
2017-11-02 12:32:40 +0000

    Exclude most of erlang when making a portable Erlang for the converge escript

--- lib/portable_erlang.ex
+++ lib/portable_erlang.ex
@@ -3,11 +3,24 @@ defmodule MachineManager.PortableErlang do
 	Create a portable Erlang installation at `dest` (must be an already-existing empty directory)
 	"""
 	def make_portable_erlang(dest) do
+		erlang_base = "/usr/lib/erlang"
 		{:ok, []} = File.ls(dest)
-		# TODO: exclude things not needed to run converge scripts
-		{"", 0} = System.cmd("rsync", ["-a", "--", "/usr/lib/erlang/", dest], stderr_to_stdout: true)
-		erl_file = Path.join(dest, "bin/erl")
-		# Make bin/erl work when installed to any location
+		erlang_libs   = File.ls!("#{erlang_base}/lib")
+		unwanted_libs = Enum.reject(erlang_libs, fn lib -> lib =~ ~r/\A(kernel|stdlib|compiler)-/ end)
+		excludes      = [
+			"/erts-*/include",
+			"/erts-*/lib",
+			"/erts-*/bin/dialyzer",
+			"/erts-*/bin/epmd",
+			"/erts-*/bin/heart",
+		] ++ Enum.map(unwanted_libs, fn lib -> "/lib/#{lib}" end)
+		exclude_args = Enum.flat_map(excludes, fn path -> ["--exclude", path] end)
+		{"", 0} = System.cmd("rsync", exclude_args ++ ["-a", "--", "#{erlang_base}/", dest], stderr_to_stdout: true)
+		fix_bin_erl(Path.join(dest, "bin/erl"))
+	end
+
+	# Make bin/erl work when installed to any location
+	defp fix_bin_erl(erl_file) do
 		erl_content =
 			File.read!(erl_file)
 			|> String.replace(
