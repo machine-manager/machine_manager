a15bea8f1059e65e9cff7e723d6dc75b969798f8
Ivan Kozik <ivan@ludios.org>
2017-11-02 12:54:50 +0000

    make_portable_erlang: exclude all the parts of Erlang we don't need to run a converge escript

--- lib/portable_erlang.ex
+++ lib/portable_erlang.ex
@@ -3,17 +3,26 @@ defmodule MachineManager.PortableErlang do
 	Create a portable Erlang installation at `dest` (must be an already-existing empty directory)
 	"""
 	def make_portable_erlang(dest) do
-		erlang_base = "/usr/lib/erlang"
-		{:ok, []} = File.ls(dest)
-		erlang_libs   = File.ls!("#{erlang_base}/lib")
-		unwanted_libs = Enum.reject(erlang_libs, fn lib -> lib =~ ~r/\A(kernel|stdlib|compiler)-/ end)
-		excludes      = [
-			"/erts-*/include",
-			"/erts-*/lib",
-			"/erts-*/bin/dialyzer",
-			"/erts-*/bin/epmd",
-			"/erts-*/bin/heart",
-		] ++ Enum.map(unwanted_libs, fn lib -> "/lib/#{lib}" end)
+		erlang_base        = "/usr/lib/erlang"
+		{:ok, []}          = File.ls(dest)
+		[erts]             = Path.wildcard("#{erlang_base}/erts-*")
+		erlang_libs        = File.ls!("#{erlang_base}/lib")
+		erlang_bins        = File.ls!("#{erlang_base}/bin")
+		erlang_erts_bins   = File.ls!("#{erts}/bin")
+		unwanted_libs      = Enum.reject(erlang_libs,      fn n -> n =~ ~r/\A(kernel|stdlib|compiler)-/ end)
+		unwanted_bins      = Enum.reject(erlang_bins,      fn n -> n =~ ~r/\A(erl|escript|start_clean\.boot)\z/ end)
+		unwanted_erts_bins = Enum.reject(erlang_erts_bins, fn n -> n =~ ~r/\A(erlexec|erl_child_setup|inet_gethost|beam\.smp)\z/ end)
+		excludes           =
+			Enum.map(unwanted_libs,      fn n -> "/lib/#{n}" end) ++
+			Enum.map(unwanted_bins,      fn n -> "/bin/#{n}" end) ++
+			Enum.map(unwanted_erts_bins, fn n -> "/erts-*/bin/#{n}" end) ++
+			[
+				"/usr",
+				"/include",
+				"/releases",
+				"/erts-*/lib",
+				"/erts-*/include",
+			]
 		exclude_args = Enum.flat_map(excludes, fn path -> ["--exclude", path] end)
 		{"", 0} = System.cmd("rsync", exclude_args ++ ["-a", "--", "#{erlang_base}/", dest], stderr_to_stdout: true)
 		fix_bin_erl(Path.join(dest, "bin/erl"))
